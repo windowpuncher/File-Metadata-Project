e6b6d072cb74572a7bdd4cb470251c898e7d235a
windowpuncher <jakestorkamp@yahoo.com>
2020-12-14 17:34:23 -0600

    Works and passes tests, and as a bonus, manages its own storage

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
+require('dotenv').config()
 var express = require('express');
 var cors = require('cors');
-require('dotenv').config()
+var bodyParser = require("body-parser");
+var multer = require("multer");
+var fs = require("fs");
+var upload = multer({ dest: "./public/data/uploads/" });
 
 var app = express();
 
@@ -8,13 +12,29 @@ app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
 
 app.get('/', function (req, res) {
-    res.sendFile(process.cwd() + '/views/index.html');
+  res.sendFile(process.cwd() + '/views/index.html');
 });
-
-
-
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
   console.log('Your app is listening on port ' + port)
+});
+
+app.use("/", bodyParser.urlencoded({ extended: false }));
+
+app.post("/api/fileanalyse", upload.single("upfile"), (req, res) => {
+  let info = {
+    "name": req.file.originalname,
+    "type": req.file.mimetype,
+    "size": req.file.size
+  }
+
+  let path = req.file.path;
+
+  fs.unlink(path, (err) => {
+    if (err) console.error(err);
+    // console.log("Path/file was deleted.");
+  });
+
+  res.json(info);
 });
